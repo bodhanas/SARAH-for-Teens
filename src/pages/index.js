188afdd412bf693305f61ba32d6b8b5dd3d42e3c
bodhana2004 <63922812+bodhana2004@users.noreply.github.com>
2020-06-01 08:41:15 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const IndexPage = () => (
     <SEO title="Home" />
     <h1>Sports and Recreation at Home</h1>
     <p style = {{marginBottom:'20px'}}>Discover how to keep your passion for sports and recreation alive in the midst of COVID-19 with SARAH.</p>
-    <img style = {{marginBottom:'0px'}} src="https://lh3.googleusercontent.com/proxy/XmJlWj9sdyJW4ROTPeXdgi3ulRR1yqRLa7P7ppdZp3jpWEW7e8cXodzWQ90rjgB-QFMkPxEmO3JhokXxiaWREZGGlUJgK_4ea-sEoNzEXssA_xxaFDns-ltOvQ" alt="" width="300"
+    <img style = {{marginBottom:'0px'}} src="https://lh3.googleusercontent.com/proxy/PpEevVwG3XVwzd4h0Zy5HvZWKx2zSZiWh39TNMia8FRe_zi_n7kCIMIUZ2gf2B85vr0r6er4riXeTuu4neHniJ7nqOoZxJ9R8MI_HlvWHHS3QDvoR9qM5NtA4w" alt="" width="300"
          height="160"></img>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
     </div>
